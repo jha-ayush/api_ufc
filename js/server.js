ebc45481c42cb3d910563a05eb72ef9cef338a0b
Ayush Jha <ayushjha@Ayushs-MacBook-Pro.local>
2017-03-10 21:52:24 -0800

    display

--- js/server.js
+++ js/server.js
@@ -1,1 +1,20 @@
 console.log("Hello from server.js");
+
+
+var api_path = 'http://ufc-data-api.ufc.com/api/v1/us/fighters';
+
+function setup() {
+	loadJSON(api_path, gotData, 'jsonp');
+}
+
+function gotData (data) {
+	console.log(data);
+    // mmaData = data;
+}
+
+function action() {
+  if (mmaData) {
+    console.log(mmaData.main.first_name);
+    console.log(mmaData.main.last_name);
+  }
+}
